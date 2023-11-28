e14c249803c727f9b8a6e7c792a21ab992440a5c
kali <kali@kali.kali>
2023-11-28 09:11:07 -0600

    level6 and level7

---
+++ level6/main.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+
+        char old_string[] = "terminate";
+    int new_string; // [esp+18h] [ebp-10h]
+
+    old_string[strcspn(old_string, "\n")] = 0;
+
+        new_string = (old_string[3] ^ 0x1337) + 6221293;
+        for ( int i = 0; i < strnlen(old_string, 32); ++i )
+        {
+        if ( old_string[i] <= 31 )
+            return 1;
+        new_string += (new_string ^ (unsigned int)old_string[i]) % 0x539;
+        }
+    printf("%d", new_string);
+}
--- level6/source.c
+++ level6/source.c
@@ -1,34 +1,49 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-
-void n(void)
-
+_BOOL4 __cdecl auth(char *login, int serial)
 {
-  system("/bin/cat /home/user/level7/.pass");
-  return;
+  int new_string; // [esp+18h] [ebp-10h]
+
+  login[strcspn(login, "\n")] = 0;
+
+  if ( strnlen(login, 32)<= 5 )
+    return 1;
+  else if ( ptrace(PTRACE_TRACEME, 0, 1, 0) == -1 )
+  {
+    puts("\x1B[32m.---------------------------.");
+    puts("\x1B[31m| !! TAMPERING DETECTED !!  |");
+    puts("\x1B[32m'---------------------------'");
+    return 1;
+  }
+  else
+  {
+    new_string = (login[3] ^ 0x1337) + 6221293;
+    for ( int i = 0; i < strnlen(login, 32); ++i )
+    {
+      if ( login[i] <= 31 )
+        return 1;
+      new_string += (new_string ^ (unsigned int)login[i]) % 0x539;
+    }
+    return serial != new_string;
+  }
 }
 
-void m(void)
-
+int __cdecl main(int argc, const char **argv, const char **envp)
 {
-  puts("Nope");
-  return;
-}
+  int serial;
+  char login[28];
 
-
-
-void main(int ac, char *av[])
-
-{
-  char *dest;
-void (*func)(void);
-  
-  dest = (char *)malloc(64);
-  func = malloc(4);
-  func = &m;
-  strcpy(dest, av[1]);
-  (func)();
-  return;
+  puts("***********************************");
+  puts("*\t\tlevel06\t\t  *");
+  puts("***********************************");
+  printf("-> Enter Login: ");
+  fgets(login, 32, stdin);
+  puts("***********************************");
+  puts("***** NEW ACCOUNT DETECTED ********");
+  puts("***********************************");
+  printf("-> Enter Serial: ");
+  scanf(&unk_8048A60, &serial);
+  if ( auth(login, serial) )
+    return 1;
+  puts("Authenticated!");
+  system("/bin/sh");
+  return 0;
 }
